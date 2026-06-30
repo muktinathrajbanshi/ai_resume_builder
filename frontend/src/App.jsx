115c8e5643726097d508b4e6dd888bcee7df9019
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-30 22:17:30 +0545

    patlu kuch kar mujhe nid lagi hain

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="app" element={<Layout />}></Route>
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
       </Routes>
     </>
   );
