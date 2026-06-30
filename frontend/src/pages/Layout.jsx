8ae1f8fa814bf71117fc6401164bc0dcbc1d30fd
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-30 22:33:05 +0545

    Ye kiya horaha hain mere saath umm aah

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
   return (
     <div>
       <h1>Layout Page</h1>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 };
