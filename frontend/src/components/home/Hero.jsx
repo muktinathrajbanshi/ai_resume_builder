20cfc7d66056e5cf83f36b07ce2f07491b52c58e
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-01 21:53:41 +0545

    mamau jaldi karo

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -135,16 +135,16 @@ const Hero = () => {
           <div className="flex gap-2">
             <Link
               to="/app?state=register"
-              className="hidden md:block px-6 py-2 bg-green-500 hover:bg-indigo-700 active:scale-95 transition-all rounded-full text-white"
+              className="hidden md:block px-6 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white"
             >
               Get started
             </Link>
-            <a
-              href=""
+            <Link
+              to="/app?state=login"
               className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900"
             >
               Login
-            </a>
+            </Link>
           </div>
 
           <button
