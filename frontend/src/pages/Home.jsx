c936cc25bf271233cdd6cdaf8205a06963765552
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-01 21:13:15 +0545

    la yar aja ta jhan dhilo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/home/Banner";
+import Hero from "../components/home/Hero";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Hero />
     </div>
   );
 };
