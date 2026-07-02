f8ef5059d73e3f0697fdb9096f10c38ab4986ebd
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-02 15:50:42 +0545

    wah kiya baat hain

--- frontend/src/components/home/Features.jsx
+++ frontend/src/components/home/Features.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Zap } from "lucide-react";
+import Title from "./Title";
 
 const Features = () => {
   const [isHover, setIsHover] = React.useState(false);
@@ -13,8 +14,12 @@ const Features = () => {
         <Zap width={14} />
         <span>Simple Process</span>
       </div>
+      <Title
+        title="Build your resume"
+        description="Our streamlined process helps you create a professional resume in minutes with intelligent AI-powered tools and features."
+      />
 
-      <div className="flex flex-col md:flex-row items-center justify-center">
+      <div className="flex flex-col md:flex-row items-center justify-center xl:-mt-10">
         <img
           className="max-w-2xl w-full xl:-ml-32"
           src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png"
