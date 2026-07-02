c33f20df58344af738203df2369f89917f63eb90
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-02 21:46:33 +0545

    Hait kasto din audai chha yar

--- frontend/src/components/home/Testimonial.jsx
+++ frontend/src/components/home/Testimonial.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Title from "./Title";
+import { BookUserIcon } from "lucide-react";
 
 const Testimonial = () => {
   return (
@@ -7,8 +9,8 @@ const Testimonial = () => {
       className="flex flex-col items-center my-10 scroll-mt-12"
     >
       <div className="flex items-center gap-2 text-sm text-green-600 bg-green-400/10 rounded-full px-6 py-1.5">
-        <Zap width={14} />
-        <span>Simple Process</span>
+        <BookUserIcon className="size-4.5 stroke-green-600" />
+        <span>Testimonials</span>
       </div>
       <Title
         title="Build your resume"
