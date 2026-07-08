016841281df241ee005146440625391882c1f730
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-08 13:01:44 +0545

    hait yar ekchhin parkhi

--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -1,4 +1,11 @@
+import { useParams, Link } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import { useEffect, useState } from "react";
+import { ArrowLeftIcon, Briefcase, FileText, User } from "lucide-react";
+
 const ResumeBuilder = () => {
+  const { resumeId } = useParams();
+
   const [resumeData, setResumeData] = useState({
     _id: "",
     title: "",
@@ -13,7 +20,45 @@ const ResumeBuilder = () => {
     public: false,
   });
 
-  return <div></div>;
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+
+  const sections = [
+    { id: "personal", name: "Personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "experience", name: "Experience", icon: Briefcase },
+  ];
+
+  useEffect(() => {
+    loadExistingResume();
+  }, []);
+
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link
+          to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all"
+        >
+          <ArrowLeftIcon className="size-4" />
+          Back to Dashboard
+        </Link>
+      </div>
+      <div className="max-w-7xl mx-auto px-4 pb-8">
+        <div className="grid lg:grid-cols-12 gap-8">
+          {/* Left Panel - Form  */}
+          <div></div>
+          {/* Right Panel - Preview  */}
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeBuilder;
