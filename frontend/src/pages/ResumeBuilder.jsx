a6c411a0e15758e9f8e4f2089f4bab43507c67f5
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-08 12:31:51 +0545

    mummy aah aah kaun sa musibat aal pade le haha

--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -1,10 +1,19 @@
-
 const ResumeBuilder = () => {
-  return (
-    <div>
-      <h1>ResumeBuilder Page</h1>
-    </div>
-  )
-}
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  });
 
-export default ResumeBuilder
+  return <div></div>;
+};
+
+export default ResumeBuilder;
