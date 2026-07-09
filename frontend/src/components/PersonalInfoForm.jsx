265bf04188902c12d925d096ca0c145d51f3202c
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-09 19:03:28 +0545

    theherja theherja keheta hain dil ye mera

--- frontend/src/components/PersonalInfoForm.jsx
+++ frontend/src/components/PersonalInfoForm.jsx
@@ -46,7 +46,16 @@ const PersonalInfoForm = ({
         {typeof data.image === "object" && (
           <div className="flex flex-col gap-1 pl-4 text-sm">
             <p>Remove Background</p>
-            <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3"></label>
+            <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                onChange={() => setRemoveBackground((prev) => !prev)}
+                checked={removeBackGround}
+              />
+              <div className="w-9 h-5 bg-slate-300 rounded-full peer peer-checked:bg-green-600 transition-colors duration-200"></div>
+              <span className="dot absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-4"></span>
+            </label>
           </div>
         )}
       </div>
