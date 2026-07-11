9b11eec05ac6b4bcc3cf73f787196176ffb6914f
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-11 21:53:48 +0545

    la mitho nidra lago my lord

--- frontend/src/components/TemplateSelector.jsx
+++ frontend/src/components/TemplateSelector.jsx
@@ -1,7 +1,7 @@
-import { Layout } from "lucide-react";
+import { Check, Layout } from "lucide-react";
 import { useState } from "react";
 
-const TemplateSelector = ({ selectedTemplated, onChange }) => {
+const TemplateSelector = ({ selectedTemplate, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const templates = [
@@ -47,7 +47,22 @@ const TemplateSelector = ({ selectedTemplated, onChange }) => {
                 setIsOpen(false);
               }}
               className={`relative p-3 border rounded-md cursor-pointer transition-all ${selectedTemplate === template.id ? "border-blue-400 bg-blue-100" : "border-gray-300 hover:border-gray-400 hover:bg-gray-100"}`}
-            ></div>
+            >
+              {selectedTemplate === template.id && (
+                <div className="absolute top-2 right-2">
+                  <div className="size-5 bg-blue-400 rounded-full flex items-center justify-center">
+                    <Check className="w-3 h-3 text-white" />
+                  </div>
+                </div>
+              )}
+
+              <div>
+                <h4 className="font-medium text-gray-800">{template.name}</h4>
+                <div className="mt-2 p-2 bg-blue-50 rounded text-xs text-gray-500 italic">
+                  {template.preview}
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       )}
