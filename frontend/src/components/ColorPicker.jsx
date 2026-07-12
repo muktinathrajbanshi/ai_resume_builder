a605eafe8f44a5ffe58045b49fb9369afcfaead6
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-12 19:01:08 +0545

    wow it's a brilliant idea muktinath

--- frontend/src/components/ColorPicker.jsx
+++ frontend/src/components/ColorPicker.jsx
@@ -1,4 +1,7 @@
-const ColorPicker = () => {
+import { Palette } from "lucide-react";
+import { useState } from "react";
+
+const ColorPicker = ({ selectedColor, onChange }) => {
   const colors = [
     { name: "Blue", value: "#3B82F6" },
     { name: "Indigo", value: "#6366F1" },
@@ -12,7 +15,15 @@ const ColorPicker = () => {
     { name: "Black", value: "#1F2937" },
   ];
 
-  return <div></div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      <button className="flex items-center gap-1 text-sm text-purple-600 bg-gradient-to-br from-purple-50 to-purple-100 ring-purple-300 hover:ring transition-all px-3 py-2 rounded-lg">
+        <Palette size={16} /> <span className="max-sm:hidden">Accent</span>
+      </button>
+    </div>
+  );
 };
 
 export default ColorPicker;
