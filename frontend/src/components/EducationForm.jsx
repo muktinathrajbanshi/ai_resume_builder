9089eef0d7904bfee9278ee143172a5b2318676c
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-15 20:24:09 +0545

    kati saro nidra lageko yar

---
+++ frontend/src/components/EducationForm.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const EducationForm = ({ data, onChange }) => {
+  const addExperience = () => {
+    const newExperience = {
+      company: "",
+      position: "",
+      start_date: "",
+      end_date: "",
+      description: "",
+      is_current: false,
+    };
+    onChange([...data, newExperience]);
+  };
+
+  const removeExperience = (index) => {
+    const updated = data.filter((_, i) => i !== index);
+    onChange(updated);
+  };
+
+  const updateExperience = (index, field, value) => {
+    const updated = [...data];
+    updated[index] = { ...updated[index], [field]: value };
+    onChange(updated);
+  };
+
+  return <div></div>;
+};
+
+export default EducationForm;
