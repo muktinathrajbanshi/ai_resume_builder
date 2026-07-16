d6baf8bdca5c2370c7c8c7f5e733ae68a2093509
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-16 21:24:45 +0545

    la nidra lagna thalo man

--- frontend/src/components/ProjectForm.jsx
+++ frontend/src/components/ProjectForm.jsx
@@ -20,21 +20,21 @@ const ProjectForm = ({ data, onChange }) => {
   };
 
   return (
-    <div className="space-y-6">
+    <div>
       <div className="flex items-center justify-between">
         <div>
           <h3 className="flex items-center gap-2 text-lg font-semibold text-gray-900">
-            Education
+            Projects
           </h3>
-          <p className="text-sm text-gray-500">Add your education details</p>
+          <p className="text-sm text-gray-500">Add your projects</p>
         </div>
         <button
-          onClick={addEducation}
+          onClick={addProject}
           className="flex items-center gap-2 px-3 py-1 text-sm bg-green-100 text-green-700 rounded-lg
         hover:bg-green-200 transition-colors"
         >
           <Plus className="size-4" />
-          Add Education
+          Add Project
         </button>
       </div>
 
