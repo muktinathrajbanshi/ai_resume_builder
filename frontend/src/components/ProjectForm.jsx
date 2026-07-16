bae33403c253d245cc62fc7fa4cf390a64d82d38
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-16 21:12:52 +0545

    la chhito sak ta vai

---
+++ frontend/src/components/ProjectForm.jsx
@@ -0,0 +1,27 @@
+const ProjectForm = ({ data, onChange }) => {
+  const addEducation = () => {
+    const newEducation = {
+      institution: "",
+      degree: "",
+      field: "",
+      graduation_date: "",
+      gpa: "",
+    };
+    onChange([...data, newEducation]);
+  };
+
+  const removeEducation = (index) => {
+    const updated = data.filter((_, i) => i !== index);
+    onChange(updated);
+  };
+
+  const updateEducation = (index, field, value) => {
+    const updated = [...data];
+    updated[index] = { ...updated[index], [field]: value };
+    onChange(updated);
+  };
+
+  return <div></div>;
+};
+
+export default ProjectForm;
