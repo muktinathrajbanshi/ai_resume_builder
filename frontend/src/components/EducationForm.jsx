1b5553f687abcddea6819355cfd37c536db11973
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-16 21:01:12 +0545

    ade vidu gopal aaiya

--- frontend/src/components/EducationForm.jsx
+++ frontend/src/components/EducationForm.jsx
@@ -1,4 +1,4 @@
-import { GraduationCap } from "lucide-react";
+import { GraduationCap, Plus, Trash2 } from "lucide-react";
 import React from "react";
 
 const EducationForm = ({ data, onChange }) => {
