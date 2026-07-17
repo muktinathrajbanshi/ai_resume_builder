862a7b876cc66db2d876df306bf3697940c870e4
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-17 20:43:51 +0545

    la chhito sak la

--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -19,6 +19,7 @@ import ColorPicker from "../components/ColorPicker";
 import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
+import ProjectForm from "../components/ProjectForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -179,6 +180,15 @@ const ResumeBuilder = () => {
                     data={resumeData.education}
                     onChange={(data) =>
                       setResumeData((prev) => ({ ...prev, education: data }))
+                    }
+                  />
+                )}
+
+                {activeSection.id === "projects" && (
+                  <ProjectForm
+                    data={resumeData.project}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({ ...prev, project: data }))
                     }
                   />
                 )}
