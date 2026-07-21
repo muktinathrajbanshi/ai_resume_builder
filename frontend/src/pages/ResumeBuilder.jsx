ad22b591904ba60419731fdb6fbaa8226de5b328
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-21 21:57:57 +0545

    wow it's working perfect

--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -84,6 +84,10 @@ const ResumeBuilder = () => {
     }
   };
 
+  const downloadResume = () => {
+    window.print();
+  };
+
   return (
     <div>
       <div className="max-w-7xl mx-auto px-4 py-6">
@@ -234,11 +238,17 @@ const ResumeBuilder = () => {
             <div className="relative w-full">
               <div className="absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2">
                 {resumeData.public && (
-                  <button className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors">
+                  <button
+                    onClick={handleShare}
+                    className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors"
+                  >
                     <Share2Icon className="size-4" />
                   </button>
                 )}
-                <button className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors">
+                <button
+                  onClick={changeResumeVisibility}
+                  className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors"
+                >
                   {resumeData.public ? (
                     <EyeIcon className="size-4" />
                   ) : (
@@ -246,7 +256,10 @@ const ResumeBuilder = () => {
                   )}
                   {resumeData.public ? "Public" : "Private"}
                 </button>
-                <button className="flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors">
+                <button
+                  onClick={downloadResume}
+                  className="flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors"
+                >
                   <DownloadIcon className="size-4" /> Download
                 </button>
               </div>
