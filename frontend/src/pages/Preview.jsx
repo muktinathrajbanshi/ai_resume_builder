6f5cf10e44c6130db8d05782952e493e57fe0700
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-21 22:27:05 +0545

    Jay ma rastra

--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
+import { dummyResumeData } from "../assets/assets";
 
 const Preview = () => {
   const { resumeId } = useParams();
   const [resumeData, setResumeData] = useState(null);
+
+  const loadResume = async () => {
+    setResumeData(
+      dummyResumeData.find((resume) => resume._id === resumeId || null),
+    );
+  };
+
   return (
     <div>
       <h1>Preview Page</h1>
