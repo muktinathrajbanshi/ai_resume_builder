09665e95afccb3646500c8755fa8d49f8144f00a
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-21 22:29:35 +0545

    hey bhagban kina yesto feri

--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/assets";
+import { useParams } from "react-router-dom";
 
 const Preview = () => {
   const { resumeId } = useParams();
@@ -10,6 +11,10 @@ const Preview = () => {
       dummyResumeData.find((resume) => resume._id === resumeId || null),
     );
   };
+
+  useEffect(() => {
+    loadResume();
+  }, []);
 
   return (
     <div>
