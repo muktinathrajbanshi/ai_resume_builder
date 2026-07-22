ba691571e4f6f2c6b5208932c13a6b7a5227bb02
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-22 21:24:23 +0545

    wow

--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -2,14 +2,18 @@ import { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/assets";
 import { useParams } from "react-router-dom";
 import ResumePreview from "../components/ResumePreview";
+import Loader from "../components/loader";
 
 const Preview = () => {
   const { resumeId } = useParams();
+
+  const [isLoading, setIsLoading] = useState(true);
   const [resumeData, setResumeData] = useState(null);
 
   const loadResume = async () => {
     setResumeData(
       dummyResumeData.find((resume) => resume._id === resumeId || null),
+      setIsLoading(false),
     );
   };
 
@@ -17,17 +21,20 @@ const Preview = () => {
     loadResume();
   }, []);
 
-
-
   return resumeData ? (
     <div className="bg-slate-100">
       <div className="max-w-3xl mx-auto py-10">
-        <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} classes="py-4 bg-white" />
+        <ResumePreview
+          data={resumeData}
+          template={resumeData.template}
+          accentColor={resumeData.accent_color}
+          classes="py-4 bg-white"
+        />
       </div>
     </div>
   ) : (
-
-  )
+    <div>{isLoading ? <Loader /> : <div></div>}</div>
+  );
 };
 
 export default Preview;
