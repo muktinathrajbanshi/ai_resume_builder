516512bd8e3b96b24915255e7e7ac5caf86e68ba
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-22 21:33:15 +0545

    wow muktinath it's perfect

--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -42,7 +42,10 @@ const Preview = () => {
           <p className="text-center text-6xl text-slate-400 font-medium">
             Resume not found
           </p>
-          <a href="">
+          <a
+            href=""
+            className="mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors"
+          >
             <ArrowLeftIcon className="mr-2 size-4" /> go to home page
           </a>
         </div>
