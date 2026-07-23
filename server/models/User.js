4687a927961045bc0b69f7e136a8247edd979b0c
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-23 22:21:55 +0545

    wow kati chhito navako

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -8,3 +9,11 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
