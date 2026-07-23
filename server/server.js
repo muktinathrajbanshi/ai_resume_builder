ea2335a359fa0c0fe91b646db05a8ddeafd66719
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-23 20:58:12 +0545

    halka dhilo vayo yar

--- server/server.js
+++ server/server.js
@@ -4,3 +4,12 @@ import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (res, res) => res.send("Server is live..."));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
