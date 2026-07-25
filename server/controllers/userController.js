7ff3b7e183815722b048f14277707cd221c215ae
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-25 21:28:32 +0545

    La nidra lago oi nidra lago

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -63,21 +63,13 @@ export const loginUser = async (req, res) => {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
-    // create new user
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
     // return success message
-    const token = generateToken(newUser._id);
-    newUser.password = undefined;
+    const token = generateToken(user._id);
+    user.password = undefined;
 
     return res
-      .status(201)
-      .json({ message: "User created successfully", token, user: newUser });
+      .status(200)
+      .json({ message: "Login successful", token, user: user });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
