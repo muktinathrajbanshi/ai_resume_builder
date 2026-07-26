fdd2c4080e334918d7ad4d38ade85542aac12128
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-26 21:19:12 +0545

    hey vagban kaile saknu yo

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  getUserById,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
+import protect from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/data", protect, getUserById);
+
+export default userRouter;
