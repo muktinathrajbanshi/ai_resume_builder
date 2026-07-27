115a890a611d03f0f5c3d6dcf3c60ed0b3fbfa38
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-27 22:42:41 +0545

    aja lai yati la

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -56,3 +56,12 @@ export const getResumeById = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
+
+// get resume by id public
+// GET: /api/resumes/public
+export const getPublicResumeById = async (req, res) => {
+  try {
+    const { resumeId } = req.params;
+    const resume = await Resume.findOne({ public: true, _id: resumeId });
+  } catch (error) {}
+};
