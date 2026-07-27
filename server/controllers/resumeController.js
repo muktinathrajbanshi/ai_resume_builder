3009538f88a4f2da0c0258cd651e0d5ca404e99f
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-27 22:29:00 +0545

    chhito gar muktinath

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -9,6 +9,26 @@ export const createResume = async (req, res) => {
 
     // create new resume
     const newResume = await Resume.create({ userId, title });
+    // return success message
+    return res
+      .status(201)
+      .json({ message: "Resume created successfully", resume: newResume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for deleting a resume
+// DELETE: /api/resumes/delete
+export const deleteResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    await Resume.findOneAndDelete({ userId, _id: resumeId });
+    return res
+      .status(201)
+      .json({ message: "Resume created successfully", resume: newResume });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
