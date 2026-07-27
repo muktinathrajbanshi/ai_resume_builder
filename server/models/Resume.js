9f33a7021dc64c763b1bc3dd6b11c312c27b1c12
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-27 19:37:34 +0545

    let's get started on this

--- server/models/Resume.js
+++ server/models/Resume.js
@@ -8,5 +8,21 @@ const resumeSchema = new mongoose.Schema({
   accent_color: { type: String, default: "#3B82F6" },
   professional_summary: { type: String, default: "" },
   skills: [{ type: String }],
-  personal_info: {},
+  personal_info: {
+    image: { type: String, default: "" },
+    full_name: { type: String, default: "" },
+    profession: { type: String, default: "" },
+    email: { type: String, default: "" },
+    phone: { type: String, default: "" },
+    location: { type: String, default: "" },
+    linkedin: { type: String, default: "" },
+    website: { type: String, default: "" },
+  },
+  experience: [
+    {
+      company: { type: String },
+      position: { type: String },
+      start_date: { type: String },
+    },
+  ],
 });
