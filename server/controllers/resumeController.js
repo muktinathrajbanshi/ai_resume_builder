8269c1cc995168778511a72066f24e80b7bf505c
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-28 21:20:27 +0545

    la yar aja ta jhanai dhilo

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -63,5 +63,13 @@ export const getPublicResumeById = async (req, res) => {
   try {
     const { resumeId } = req.params;
     const resume = await Resume.findOne({ public: true, _id: resumeId });
-  } catch (error) {}
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json({ resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
