2d306071dedcaf8d19922828bb60d9746a6e8835
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-28 21:32:27 +0545

    nidra lago my lord

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -81,6 +81,16 @@ export const getPublicResumeById = async (req, res) => {
     const userId = req.userId;
     const { resumeId, resumeData, removeBackground } = req.body;
     const image = req.file;
+
+    let resumeDataCopy = JSON.parse(resumeData);
+
+    const resume = await Resume.findByIdAndUpdate(
+      { userId, _id: resumeId },
+      resumeDataCopy,
+      { new: true },
+    );
+
+    return res.status(200).json({ message: "Saved successfully", resume });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
