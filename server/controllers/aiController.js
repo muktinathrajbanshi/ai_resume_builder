1ee534a97abba18b7d88b7754577994205519070
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-30 22:33:26 +0545

    wow backend also completed now it's time for integrating both frontend and backend

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,7 +1,7 @@
+import ai from "../configs/ai.js";
+import Resume from "../models/Resume.js";
+
 // controller for enhancing a resume's professional summary
-
-import { response } from "express";
-
 // POST: /api/ai/enhance-pro-sm
 export const enhanceProfessionalSummary = async (req, res) => {
   try {
@@ -79,7 +79,51 @@ export const uploadResume = async (req, res) => {
     const systemPrompt =
       "You are an expert AI Agent to extract data from resume.";
 
-    const userPrompt = `extract data from this resume: ${resumeText}`;
+    const userPrompt = `extract data from this resume: ${resumeText}
+    
+    Provide data in the following JSON format with no additional text before or after:
+
+    {
+      professional_summary: { type: String, default: "" },
+      skills: [{ type: String }],
+      personal_info: {
+        image: { type: String, default: "" },
+        full_name: { type: String, default: "" },
+        profession: { type: String, default: "" },
+        email: { type: String, default: "" },
+        phone: { type: String, default: "" },
+        location: { type: String, default: "" },
+        linkedin: { type: String, default: "" },
+        website: { type: String, default: "" },
+      },
+      experience: [
+        {
+          company: { type: String },
+          position: { type: String },
+          start_date: { type: String },
+          end_date: { type: String },
+          description: { type: String },
+          is_current: { type: Boolean },
+        },
+      ],
+      project: [
+        {
+          name: { type: String },
+          type: { type: String },
+          description: { type: String },
+        },
+      ],
+      education: [
+        {
+          institution: { type: String },
+          degree: { type: String },
+          field: { type: String },
+          graduation_date: { type: String },
+          gpa: { type: String },
+        },
+      ],
+    }
+    `;
 
     const response = await ai.chat.completions.create({
       model: process.env.OPENAI_MODEL,
@@ -96,8 +140,11 @@ export const uploadResume = async (req, res) => {
       response_format: { type: "json_object" },
     });
 
-    const enhancedContent = response.choices[0].message.content;
-    return res.status(200).json({ enhancedContent });
+    const extractedData = response.choices[0].message.content;
+    const parseData = JSON.parse(extractedData);
+    const newResume = await Resume.create({ userId, title, ...parseData });
+
+    res.json({ resumeId: newResume._id });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,5 +1,5 @@
 import imagekit from "../configs/imageKit.js";
-import Resume from "../models/Resume";
+import Resume from "../models/Resume.js";
 import fs from "fs";
 
 // controller for creating a new resume
