206bc2d18f66af02148820521cdd7838627dd8a0
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-31 15:51:06 +0545

    oh my god it's totally brilliant

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,13 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import Loader from "../components/loader";
+import Login from "./Login";
 
 const Layout = () => {
+  const { user, loading } = useSelector((state) => state.auth);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Outlet />
-      </div>
+      {user ? (
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Outlet />
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
