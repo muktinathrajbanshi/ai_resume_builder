c3bcfe3a81a6c32bd5d90bac3e3eb2c5e294c29a
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-31 16:44:55 +0545

    wow login successful

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../app/features/authSlice";
 
 const Navbar = () => {
-  const user = { name: "Muktinath Rajbanshi" };
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
 
   const logoutUser = () => {
     navigate("/");
+    dispatch(logout());
   };
 
   return (
