c1b99dac8efc5cdfc8c7e415c6ef7df5f5e44df2
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-01 21:54:17 +0545

    wow aba kai hunchha hola

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -35,7 +35,10 @@ const Dashboard = () => {
       const { data } = await api.get("/api/users/resumes", {
         headers: { Authorization: token },
       });
-    } catch (error) {}
+      setAllResumes(data.resumes);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message);
+    }
   };
 
   const createResume = async (event) => {
